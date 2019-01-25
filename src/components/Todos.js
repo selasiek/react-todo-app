b57458e5a9e7b026c837abed11805ea0e7da5ae6
selasiek <kitsipaul@ymail.com>
2019-01-25 01:49:35 +0000

    first working commit

---
+++ src/components/Todos.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import Proptypes from 'prop-types';
+
+class Todos extends Component {
+
+  render() {
+    return this.props.todos.map((todo) => (
+    <TodoItem 
+      key={todo.id} 
+      todo={todo} 
+      markComplete={this.props.markComplete} 
+      delTodo={this.props.delTodo}
+    />
+    ));
+  }
+}
+
+//Proptypes
+Todos.Proptypes = {
+  todos: Proptypes.array.isRequired
+}
+export default Todos;
